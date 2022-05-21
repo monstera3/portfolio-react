9b96c6dd1b3d60e6a84b6403395ef5fef5ceefa8
natsumi <aka.723n@gmail.com>
2022-05-21 16:16:57 +0900

    feat: add Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import sample1 from './assets/images/sample1.png'
+import { Header } from './assets/components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={sample1} alt="sample" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
     </div>
   );
 }
---
+++ src/assets/components/Header.tsx
@@ -0,0 +1,11 @@
+import sample1 from '../images/sample1.png'
+
+export const Header = () => {
+  return(
+    <div>
+      <img src={sample1} alt="sample1"/>
+      <p>sugimura natsumi</p>
+
+    </div>
+  );
+}
