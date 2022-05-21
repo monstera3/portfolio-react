7d7e5ea91421fa242335ff2fa0970ade01b2b35e
natsumi <aka.723n@gmail.com>
2022-05-21 16:28:41 +0900

    style: add inline Styles sample

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,10 +1,18 @@
 import sample1 from '../images/sample1.png'
 
 export const Header = () => {
+
+  const containerStyle = {
+    width:"1000px",
+    color:"red",
+  };
+
   return(
     <div>
-      <img src={sample1} alt="sample1"/>
-      <p>sugimura natsumi</p>
+      <div style={containerStyle}>
+        <img src={sample1} alt="sample1"/>
+        <p>sugimura natsumi</p>
+      </div>
 
     </div>
   );
