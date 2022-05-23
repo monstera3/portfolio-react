e5382f0fcad7b52a9083cc57ccefd43e30314d40
natsumi <aka.723n@gmail.com>
2022-05-23 13:25:54 +0900

    feat: add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Top } from './assets/components/Top';
 import { Work } from './assets/components/Work';
+import { Routes,Route } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <Top />
-      <Work />
+      <Routes>
+        <Route path="/" element={<Top />}/>
+        <Route path="work" element={<Work />}/>
+      </Routes>
     </div>
   );
 }
