cae6f0b5833d67c613fe6ea57954795cb560ddc5
natsumi <aka.723n@gmail.com>
2022-05-23 14:02:35 +0900

    feat: add LeftBar.tsx

---
+++ src/assets/components/LeftBar.tsx
@@ -0,0 +1,34 @@
+import { FaGithub } from 'react-icons/fa';
+import { SiZenn } from 'react-icons/si';
+import styled from 'styled-components';
+
+export const LeftBar = () => {
+  return(
+    <LeftBarStyle>
+        <h2>Natsumi Sugimura</h2>
+        <ul>
+          <li><a href="https://www.google.com">Work</a></li>
+          <li><a href="https://www.google.com">Skill</a></li>
+          <li><a href="https://www.google.com">Profile</a></li>
+          <li><a href="https://github.com/monstera3"><FaGithub className="icon" size="2rem"/><SiZenn size="2rem" /></a></li>
+
+        </ul>
+    </LeftBarStyle>
+
+  );
+}
+
+const LeftBarStyle = styled.div`
+
+  width: 30%;
+    padding: 6rem 0;
+    text-align: center;
+    background-color: #69B0AC;
+    li{
+      list-style: none;
+      padding: 3rem 0;
+      .icon{
+        margin-right: 1rem;
+      }
+  }
+`;
--- src/assets/components/Top.tsx
+++ src/assets/components/Top.tsx
@@ -1,21 +1,11 @@
 import styled from 'styled-components';
-import { FaGithub } from 'react-icons/fa';
-import { SiZenn } from 'react-icons/si';
+import { LeftBar } from './LeftBar';
 
 export const Top = () => {
 
   return(
     <ContainerStyle>
-      <div className="logoStyle">
-        <h2>Natsumi Sugimura</h2>
-        <ul>
-          <li><a href="https://www.google.com">Work</a></li>
-          <li><a href="https://www.google.com">Skill</a></li>
-          <li><a href="https://www.google.com">Profile</a></li>
-          <li><a href="https://github.com/monstera3"><FaGithub className="icon" size="2rem"/><SiZenn size="2rem" /></a></li>
-
-        </ul>
-      </div>
+      <LeftBar />
       <div className="contentStyle">
         <p>sugimura natsumi</p>
       </div>
@@ -28,20 +18,6 @@ const ContainerStyle = styled.div`
   width:1200px;
   margin: 0 auto;
   display:flex;
-  .logoStyle{
-    padding: 6rem 0;
-    width: 30%;
-    text-align: center;
-    background-color: #69B0AC;
-    li{
-      list-style: none;
-      padding: 3rem 0;
-      .icon{
-        margin-right: 1rem;
-      }
-    }
-    
-  }
   .contentStyle{
     width: 70%;
     background-color: #F0EFE0;
