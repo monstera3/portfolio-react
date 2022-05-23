96922e4299ae8914fe82688ca8bf8c4d96eb9fad
natsumi <aka.723n@gmail.com>
2022-05-23 12:02:19 +0900

    feat: add Work.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Top } from './assets/components/Top';
+import { Work } from './assets/components/Work';
 
 function App() {
   return (
     <div className="App">
       <Top />
-
+      <Work />
     </div>
   );
 }
---
+++ src/assets/components/Work.tsx
@@ -0,0 +1,12 @@
+
+export const Work = () => {
+
+  return(
+    <>
+      <div >
+        work
+      </div>
+
+    </>
+  );
+}
