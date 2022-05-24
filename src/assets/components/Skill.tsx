890144e3ad188c38d5af69b270bb47148bf2e2d2
natsumi <aka.723n@gmail.com>
2022-05-24 16:45:48 +0900

    styke; add TODO

--- src/assets/components/Skill.tsx
+++ src/assets/components/Skill.tsx
@@ -2,6 +2,30 @@ import { LeftBar } from './LeftBar';
 import styled from 'styled-components';
 import { Grid } from '@mui/material';
 import { SkillCard } from './SkillCard';
+import React from 'react';
+import sampleImg from '../images/sample1.png';
+
+
+
+
+
+type SkillType = {
+  image: any,
+  title: string,
+}
+const skills: SkillType[] = [
+  {
+    image: {sampleImg},
+    title: 'title1',
+  },
+  {
+    image:'javascriptImg',
+    title:'title2',
+  }
+];
+
+
+
 
 export const Skill = () => {
 
@@ -14,7 +38,19 @@ export const Skill = () => {
           Skill
           <h2 className="headTitle">フロントエンド</h2>
           <h2 className="headTitle">デザイン</h2>
+
           <div className="aaa">
+
+
+            <Grid container spacing={2} maxWidth="md" m="auto">
+            {skills.map((skill) =>(
+              <Grid item xs={2} sm={2} md={2} lg={2}>
+                <SkillCard />
+              </Grid>
+            ))}
+            </Grid>
+
+
             <Grid container spacing={2} maxWidth="md" m="auto">
               <Grid item xs={2} sm={2} md={2} lg={2}>
                 <SkillCard />
--- src/assets/components/SkillCard.tsx
+++ src/assets/components/SkillCard.tsx
@@ -2,17 +2,39 @@ import { Paper } from '@mui/material';
 import React from 'react';
 import javascriptImg  from '../images/javascript.svg';
 import styled from 'styled-components';
+import sampleImg from '../images/sample1.png';
+
+
+
+
+type SkillType = {
+  image: any,
+  title: string,
+}
+const skills: SkillType[] = [
+  {
+    image: {sampleImg},
+    title: 'title1',
+  },
+  {
+    image:'javascriptImg',
+    title:'title2',
+  }
+];
+
+
+
+
 
 export const SkillCard = () => {
 
   return(
+    //TODO 画像とタイトルを入れたい
     <SkillCardStyle>
       <Paper variant="outlined" className="skillPaper">
           <img src={javascriptImg} alt=""/>
           <p>title</p>
-
       </Paper>
-
     </SkillCardStyle>
   );
 }
